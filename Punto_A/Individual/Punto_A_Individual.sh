37dccec1680101b8e4f508ff00a008a0411a648d
Cusi89 <id-cusano@hotmail.com>
2026-06-21 00:45:57 -0300

    sysadmin: Update Punto_A_Individual.sh

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -1,1 +1,13 @@
-mkdir -p ~/sysadmin/Servicios/{Web,Base_de_Datos,Cache} ~/sysadmin/Monitoreo/{Logs,Alertas,Metricas}
+#!/bin/bash
+# Script para crear la estructura de directorios individual - Rol: Sysadmin
+
+# 1. Crear la estructura principal
+mkdir -p ~/sysadmin/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+
+# 2. Crear archivos ocultos para que Git no ignore las carpetas vacías
+touch ~/sysadmin/Servicios/Web/.gitkeep
+touch ~/sysadmin/Servicios/Base_de_Datos/.gitkeep
+touch ~/sysadmin/Servicios/Cache/.gitkeep
+touch ~/sysadmin/Monitoreo/Logs/.gitkeep
+touch ~/sysadmin/Monitoreo/Alertas/.gitkeep
+touch ~/sysadmin/Monitoreo/Metricas/.gitkeep
