8286942ab02ad72c212974c9a536ec78f0031e50
Cusi89 <id-cusano@hotmail.com>
2026-06-29 00:02:43 +0000

    sysadmin: ejecutar script

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -2,12 +2,12 @@
 # Script para crear la estructura de directorios individual - Rol: Sysadmin
 
 # 1. Crear la estructura principal
-mkdir -p ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+sudo mkdir -p ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
 
 # 2. Crear archivos ocultos para que Git no ignore las carpetas vacías
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Web/.gitkeep
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Base_de_Datos/.gitkeep
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Cache/.gitkeep
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Logs/.gitkeep
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Alertas/.gitkeep
-touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Metricas/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Web/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Base_de_Datos/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Servicios/Cache/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Logs/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Alertas/.gitkeep
+sudo touch ~/TP_Final/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/sysadmin/Monitoreo/Metricas/.gitkeep
