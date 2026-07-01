b8303e13a087ff00b6bebf4421652ab47497c992
Cusi89 <id-cusano@hotmail.com>
2026-07-01 00:45:09 -0300

    dev: modificar Punto_A_Grupal_Dev.sh

--- Punto_A/Grupal/Punto_A_Grupal_Dev.sh
+++ Punto_A/Grupal/Punto_A_Grupal_Dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script para crear la estructura de directorios grupal - Rol: Sysadmin (America)
+# Script para crear la estructura de directorios grupal - Rol: Dev (Asia)
 
 # 1. Crear la estructura asimétrica de países y ciudades
 sudo mkdir -p ~/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/Continentes/Asia/{Japon/{Tokio,Kioto,Osaka},China/{Pekin,Shanghai},India/Nueva_Delhi}
