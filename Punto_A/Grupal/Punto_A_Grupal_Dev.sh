21ddd26bb0e33dc698d9b79116570f6b4e04c397
Cusi89 <id-cusano@hotmail.com>
2026-07-01 00:22:23 +0000

    dev: modificar script

---
+++ Punto_A/Grupal/Punto_A_Grupal_Dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script para crear la estructura de directorios grupal - Rol: Sysadmin (America)
+
+# 1. Crear la estructura asimétrica de países y ciudades
+sudo mkdir -p ~/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/Continentes/Asia/{Japon/{Tokio,Kioto,Osaka},China/{Pekin,Shanghai},India/Nueva_Delhi}
+
+# 2. Crear archivos ocultos para evitar que Git ignore las carpetas vacías
+sudo touch ~/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-/Continentes/Asia/{Japon/{Tokio,Kioto,Osaka},China/{Pekin,Shanghai},India/Nueva_Delhi}/.gitkeep
