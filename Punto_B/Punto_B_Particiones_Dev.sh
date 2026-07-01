24f5abe180080b7de4116d59eeff1de8ab6d7d79
Cusi89 <id-cusano@hotmail.com>
2026-07-01 00:31:14 +0000

    dev: crear script para particiones

---
+++ Punto_B/Punto_B_Particiones_Dev.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# TP Final - Arquitectura y Sistemas Operativos
+# Rol: dev - Punto B (Parte 1: Particionamiento)
+
+DISCO="/dev/sdc"
+
+echo "Iniciando particionamiento automatizado en $DISCO..."
+
+sudo fdisk ${DISCO} << EOF
+o
+n
+p
+1
+
++1G
+n
+p
+2
+
++1G
+n
+e
+3
+
+
+n
+l
+
++2.6G
+n
+l
+
++2.6G
+n
+l
+
+
+w
+EOF
+
+echo "¡Particionamiento completado con éxito!"
+sudo fdisk -l ${DISCO}
+
+sudo partprobe ${DISCO}
+
+echo "Formateando particiones en ext4..."
+sudo mkfs.ext4 /dev/sdc1
+sudo mkfs.ext4 /dev/sdc2
+sudo mkfs.ext4 /dev/sdc5
+sudo mkfs.ext4 /dev/sdc6
+sudo mkfs.ext4 /dev/sdc7
+
