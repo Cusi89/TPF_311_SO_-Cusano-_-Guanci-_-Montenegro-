77e17e1ef1a1643d88a6b73ac5aca2ee2628534c
Cusi89 <id-cusano@hotmail.com>
2026-07-01 01:28:34 +0000

    dev: modificar y ejecutar script

--- Punto_B/Punto_B_FormatMontaje_Dev.sh
+++ Punto_B/Punto_B_FormatMontaje_Dev.sh
@@ -5,11 +5,11 @@ HOME_USER="/home/vagrant/TPF_311_SO_-Cusano-_-Guanci-_-Montenegro-"
 echo "Configurando montaje persistente en /etc/fstab..."
 # Usamos un bloque append para evitar modificar manualmente el archivo
 sudo bash -c "cat << EOF >> /etc/fstab
-/dev/sdc1  ${HOME_USER}/dev/Servicios       ext4  defaults  0  2
-/dev/sdc2  ${HOME_USER}/dev/Monitoreo       ext4  defaults  0  2
-/dev/sdc5  ${HOME_USER}/dev/Servicios/Web   ext4  defaults  0  2
-/dev/sdc6  ${HOME_USER}/dev/Servicios/Cache ext4  defaults  0  2
-/dev/sdc7  ${HOME_USER}/dev/Monitoreo/Logs  ext4  defaults  0  2
+/dev/sdc1  ${HOME_USER}/Dev/Servicios       ext4  defaults  0  2
+/dev/sdc2  ${HOME_USER}/Dev/Monitoreo       ext4  defaults  0  2
+/dev/sdc5  ${HOME_USER}/Dev/Servicios/Web   ext4  defaults  0  2
+/dev/sdc6  ${HOME_USER}/Dev/Servicios/Cache ext4  defaults  0  2
+/dev/sdc7  ${HOME_USER}/Dev/Monitoreo/Logs  ext4  defaults  0  2
 EOF"
 
 echo "Aplicando montajes activos..."
